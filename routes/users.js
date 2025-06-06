9ff60d286cc57e76ff2491f4f9aa0e5a55e34c9b
sksayril <sksayril123@gmail.com>
2025-06-06 10:07:07 +0530

    chnages referl

--- routes/users.js
+++ routes/users.js
@@ -190,7 +190,7 @@ router.get('/profile', authenticateUser, async (req, res) => {
     const user = await User.findById(req.user._id).select('-password');
     
     // Generate referral links
-    const baseUrl = process.env.FRONTEND_URL || 'http://localhost:3000';
+    const baseUrl = process.env.FRONTEND_URL || 'https://utpfund.live';
     const referralLink = `${baseUrl}/register?ref=${user.referralCode}`;
     const shortLink = `${baseUrl}/r/${user.referralCode}`;
     
